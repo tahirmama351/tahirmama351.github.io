e0dda14105c91a4790d18de1385dc39753815026
tahirmama351 <tahirmama@gmail.com>
2020-09-29 22:51:31 -0500

    W2D2

--- W2_D2/getDateAgo.js
+++ W2_D2/getDateAgo.js
@@ -1,35 +1,40 @@
-function getDateAgo(date, days) {
-     date.setDate(date.getDate() - days);
-     return date.getDate();
-}
-//let date = new Date()
-//console.log(getDateAgo(date,6))
+"use strict";
+/* global assert */
+{
+  // eslint-disable-next-line require-jsdoc
+  function getDateAgo(date, days) {
+    date.setDate(date.getDate() - days);
+    return date.getDate();
+  }
+  //let date = new Date()
+  //console.log(getDateAgo(date,6))
 
 
-describe("getDateAgo", function() {
+  describe("getDateAgo", function () {
 
-     it("1 day before 02.01.2015 -> day 1", function() {
-       assert.equal(getDateAgo(new Date(2015, 0, 2), 1), 1);
-     });
-   
-   
-     it("2 days before 02.01.2015 -> day 31", function() {
-       assert.equal(getDateAgo(new Date(2015, 0, 2), 2), 31);
-     });
-   
-     it("100 days before 02.01.2015 -> day 24", function() {
-       assert.equal(getDateAgo(new Date(2015, 0, 2), 100), 24);
-     });
-   
-     it("365 days before 02.01.2015 -> day 2", function() {
-       assert.equal(getDateAgo(new Date(2015, 0, 2), 365), 2);
-     });
-   
-     it("does not modify the given date", function() {
-       let date = new Date(2015, 0, 2);
-       let dateCopy = new Date(date);
-       getDateAgo(dateCopy, 100);
-       assert.equal(date.getTime(), dateCopy.getTime());
-     });
-   
-   });
+    it("1 day before 02.01.2015 -> day 1", function () {
+      assert.equal(getDateAgo(new Date(2015, 0, 2), 1), 1);
+    });
+
+
+    it("2 days before 02.01.2015 -> day 31", function () {
+      assert.equal(getDateAgo(new Date(2015, 0, 2), 2), 31);
+    });
+
+    it("100 days before 02.01.2015 -> day 24", function () {
+      assert.equal(getDateAgo(new Date(2015, 0, 2), 100), 24);
+    });
+
+    it("365 days before 02.01.2015 -> day 2", function () {
+      assert.equal(getDateAgo(new Date(2015, 0, 2), 365), 2);
+    });
+
+    it("does not modify the given date", function () {
+      let date = new Date(2015, 0, 2);
+      let dateCopy = new Date(date);
+      getDateAgo(dateCopy, 100);
+      assert.equal(date.getTime(), dateCopy.getTime());
+    });
+
+  })
+};
--- W2_D2/maximalSalary.js
+++ W2_D2/maximalSalary.js
@@ -1,39 +1,44 @@
-function topSalary(salaries) {
+"use strict";
+/* global assert */
+{
+  // eslint-disable-next-line require-jsdoc
+  function topSalary(salaries) {
 
-     let max = 0;
-     let maxPaidName = null;
-   
-     for(const [name, salary] of Object.entries(salaries)) {
-       if (max < salary) {
-         max = salary;
-         maxPaidName = name;
-       }
-     }
-   
-     return maxPaidName;
-   }
+    let max = 0;
+    let maxPaidName = null;
 
-   let salaries = {
-     "John": 100,
-     "Pete": 300,
-     "Mary": 250
-   };
+    for (const [name, salary] of Object.entries(salaries)) {
+      if (max < salary) {
+        max = salary;
+        maxPaidName = name;
+      }
+    }
 
-   let topPaidName = topSalary(salaries)
-   console.log(topPaidName)
+    return maxPaidName;
+  }
 
-   describe("topSalary", function() {
-     it("returns top-paid person", function() {
-       let salaries = {
-         "John": 100,
-         "Pete": 300,
-         "Mary": 250
-       };
-   
-       assert.equal( topSalary(salaries), "Pete" );
-     });
-   
-     it("returns null for the empty object", function() {
-       assert.isNull( topSalary({}) );
-     });
-   });
+  let salaries = {
+    "John": 100,
+    "Pete": 300,
+    "Mary": 250
+  };
+
+  let topPaidName = topSalary(salaries)
+  console.log(topPaidName)
+
+  describe("topSalary", function () {
+    it("returns top-paid person", function () {
+      let salaries = {
+        "John": 100,
+        "Pete": 300,
+        "Mary": 250
+      };
+
+      assert.equal(topSalary(salaries), "Pete");
+    });
+
+    it("returns null for the empty object", function () {
+      assert.isNull(topSalary({}));
+    });
+  })
+};
